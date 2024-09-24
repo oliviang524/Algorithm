bbb29727b75e2c793cbfe3de2db9950ad87ea71e
oliviang524 <71427707+oliviang524@users.noreply.github.com>
2024-09-24 16:32:56 +0900

    [Bronze I] Title: 평균, Time: 196 ms, Memory: 18344 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -5,14 +5,14 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
-        int[] point = new int[N];
-        for(int i = 0; i < N ; i++) point[i] = sc.nextInt(); 
 
         long sum = 0;
         long max = 0;
+
         for(int i = 0; i < N ; i++){
-            sum += point[i];
-            if(point[i] > max) max = point[i];
+            int point = sc.nextInt();
+            sum += point;
+            if(point > max) max = point;
         }
 
         System.out.println(sum * 100.0 / max / N);
