0992a49533290cc07438779e1ef3027e9736dc66
oliviang524 <71427707+oliviang524@users.noreply.github.com>
2024-10-20 11:29:55 +0900

    [Bronze V] Title: 개수 세기, Time: 104 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int v = Integer.parseInt(br.readLine());
+        
+        int count = 0;
+        int array[] = new int[N];
+        for(int i = 0; i < N ; i++){
+            array[i] = Integer.parseInt(st.nextToken());
+            if(array[i] == v) count++;
+        }
+
+        System.out.println(count);
+        br.close(); //InputStream을 닫음
+    }
+}
