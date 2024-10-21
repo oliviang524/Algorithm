4297706faf707f756c662364fc774638f581fc07
oliviang524 <71427707+oliviang524@users.noreply.github.com>
2024-10-21 21:35:56 +0900

    [Bronze V] Title: X보다 작은 수, Time: 136 ms, Memory: 15296 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -5,14 +5,14 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
-        int N = Integer.parseInt(st1.nextToken());
-        int X = Integer.parseInt(st1.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
+        st = new StringTokenizer(br.readLine(), " ");
         int A[] = new int[N];
         for(int i = 0; i < N ; i++){
-            A[i] = Integer.parseInt(st2.nextToken());
+            A[i] = Integer.parseInt(st.nextToken());
             if(X > A[i]) sb.append(A[i]).append(" ");
         }
         System.out.println(sb.toString());
