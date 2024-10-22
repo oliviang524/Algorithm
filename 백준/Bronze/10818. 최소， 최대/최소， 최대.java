b0b5d774e6a5e45c6317f0f79f6fc2ff98b25f41
oliviang524 <71427707+oliviang524@users.noreply.github.com>
2024-10-22 18:53:04 +0900

    [Bronze III] Title: 최소, 최대, Time: 472 ms, Memory: 92192 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -7,17 +7,12 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
         int[] A = new int[N];
-        int max = 0;
-        int min = 0;
+        int max = -1000001;
+        int min = 1000001;
         for(int i = 0; i < N ; i++){
             A[i] = Integer.parseInt(st.nextToken());
-            if(i == 0){
-                max = A[i];
-                min = A[i];
-            }else{
-                if(min > A[i]) min = A[i];
-                if(max < A[i]) max = A[i];
-            }
+            if(min > A[i]) min = A[i];
+            if(max < A[i]) max = A[i];
         }
         System.out.println(min + " " + max);
         br.close();
