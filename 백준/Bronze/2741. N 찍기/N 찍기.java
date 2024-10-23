21e7aaf389cbee321864c2525850f003d50b75ec
oliviang524 <71427707+oliviang524@users.noreply.github.com>
2024-10-23 13:26:40 +0900

    [Bronze V] Title: N 찍기, Time: 188 ms, Memory: 18156 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.java
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -3,10 +3,10 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
-        for(int i = 1; i <= N ; i++){
-            System.out.println(i);
-        }
+        for(int i = 1; i <= N ; i++) sb.append(i).append("\n");
+        System.out.println(sb.toString());
         br.close();
     }
 }
