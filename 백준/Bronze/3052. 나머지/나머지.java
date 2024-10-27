4b0dd8c6a3e42a4d923b68355563cece17e5d536
oliviang524 <71427707+oliviang524@users.noreply.github.com>
2024-10-27 16:53:28 +0900

    Update 나머지.java

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,26 +1,17 @@
 import java.io.*;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int A;
-        int B = 42;
-        int C[] = new int[10];
-        for(int i = 0; i < 10; i++){
-            A = Integer.parseInt(br.readLine());
-            C[i] = A % B;
-        }
-
-        Arrays.sort(C);
+        int A = 42;
+        boolean B[] = new boolean[A];
+        for(int i = 0; i < 10; i++)
+            B[(Integer.parseInt(br.readLine())) % A] = true;
 
         int count = 0;
-        for(int i = 0; i < C.length; i++){
-            if(i == 0) count++;
-            else if(C[i - 1] == C[i]) continue;
-            else count++;
-        }
+        for(int i = 0; i < B.length; i++)
+            if(B[i]) count++;
 
         System.out.print(count);
         br.close();
