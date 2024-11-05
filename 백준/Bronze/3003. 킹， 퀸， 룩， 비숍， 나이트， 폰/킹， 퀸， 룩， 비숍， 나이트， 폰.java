b5b318674ef55a007cb9acd448d5254aadcbd0ba
oliviang524 <71427707+oliviang524@users.noreply.github.com>
2024-11-05 18:30:34 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 100 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -5,15 +5,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int A[] = {1, 1, 2, 2, 2, 8};
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");;
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         StringBuilder sb = new StringBuilder();
-        int N;
-        for(int i = 0; i < A.length ; i++){
-            N = Integer.parseInt(st.nextToken());
-            if(A[i] > N) sb.append(A[i] - N).append(" "); 
-            else if(A[i] < N) sb.append("-").append(N - A[i]).append(" "); 
-            else if(A[i] == N) sb.append(0).append(" "); 
-        }
-        System.out.print(sb.toString());
+        for(int i = 0; i < A.length ; i++) sb.append(A[i] - Integer.parseInt(st.nextToken())).append(" ");
+        System.out.println(sb.toString());
+        br.close();
     }
 }
