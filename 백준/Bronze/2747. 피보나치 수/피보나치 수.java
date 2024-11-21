48c07d7c0de163fab8571914de0936d6b587f106
oliviang524 <71427707+oliviang524@users.noreply.github.com>
2024-11-21 19:00:47 +0900

    [Bronze II] Title: 피보나치 수, Time: 108 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.java
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -3,17 +3,12 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
         int N = Integer.parseInt(br.readLine());
-
-        int A[] = new int[N + 1];
-        for(int i = 1 ; i <= N ; i++) A[i] = i;
-
-        for(int i = 2 ; i <= N + 1 ; i++){
-            if(i > N) break;
-            A[i] = (A[i - 1] + A[i - 2]);
+        int FIBONACCI = 1, TEMP = 0;
+        for(int i = 1 ; i < N ; i++){
+            FIBONACCI = FIBONACCI + TEMP;
+            TEMP = FIBONACCI - TEMP;
         }
-
-        System.out.println(A[A.length - 1]);
+        System.out.println(FIBONACCI);
     }
 }
