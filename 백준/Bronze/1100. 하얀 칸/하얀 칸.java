48a01e3d13cbdcd3ab31b360a280dfe84ba59532
oliviang524 <71427707+oliviang524@users.noreply.github.com>
2024-12-02 20:04:32 +0900

    [Bronze II] Title: 하얀 칸, Time: 104 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Bronze/1100. 하얀 칸/하얀 칸.java
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.java
@@ -1,25 +1,16 @@
 import java.io.*;
 
 public class Main {
+    private static final int N = 8;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = 8;
-        boolean chessPlate[][] = new boolean[N][N];
-
+        int white = 0;
         String S;
         for(int i = 0 ; i < N ; i++){
             S = br.readLine();
-            for(int j = 0 ; j < S.length() ; j++){
-                if(S.charAt(j) == 'F') chessPlate[i][j] = true;
-            }
-        }
-
-        int white = 0;
-        for(int i = 0 ; i < N ; i++){
-            for(int j = 0 ; j < N ; j++){
-                if(i % 2 != 0 && j % 2 != 0 && chessPlate[i][j]) white++; 
-                if(i % 2 == 0 && j % 2 == 0 && chessPlate[i][j]) white++; 
+            for(int j = i % 2 ; j < N ; j+=2){
+                if(S.charAt(j) == 'F') white++;
             }
         }
 
