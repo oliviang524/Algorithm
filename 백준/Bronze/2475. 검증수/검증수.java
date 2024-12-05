108fe93eddb3e863a08c805af08d401eb0a8fdaa
oliviang524 <71427707+oliviang524@users.noreply.github.com>
2024-12-06 08:17:56 +0900

    [Bronze V] Title: 검증수, Time: 104 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String S[] = br.readLine().split(" ");
+
+        int sum = 0;
+        for (int i = 0; i < S.length; i++) {
+            sum += Integer.parseInt(S[i]) * Integer.parseInt(S[i]);
+        }
+
+        System.out.print(sum % 10);
+    }
+}
