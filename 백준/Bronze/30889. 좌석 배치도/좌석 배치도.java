912f411509668d25aab36fcb3592055e5d49d72e
oliviang524 <71427707+oliviang524@users.noreply.github.com>
2024-12-17 09:58:17 +0900

    [Bronze III] Title: 좌석 배치도, Time: 104 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Bronze/30889. 좌석 배치도/좌석 배치도.java
+++ 백준/Bronze/30889. 좌석 배치도/좌석 배치도.java
@@ -4,22 +4,23 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        char seats[][] = new char[10][20];
+        boolean seats[][] = new boolean[10][20];
+
         int N = Integer.parseInt(br.readLine());
         String SEAT;
         for (int i = 0; i < N; i++) {
             SEAT = br.readLine();
-            seats[SEAT.charAt(0) - 64 - 1][Integer.parseInt(SEAT.substring(1, SEAT.length())) - 1] = 'o';
+            seats[SEAT.charAt(0) - 64 - 1][Integer.parseInt(SEAT.substring(1, SEAT.length())) - 1] = true;
         }
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 20; j++) {
-                if (seats[i][j] == 'o') sb.append("o");
-                else sb.append(".");
+                sb.append(seats[i][j] ? "o" : ".");
             }
             sb.append("\n");
         }
         System.out.println(sb.toString());
+
     }
 }
